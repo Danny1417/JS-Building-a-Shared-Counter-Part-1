d39eacbbfceaabd7e189145cdd588cd0c3f7d9b6
danny1417 <mr.mejia1417@gmail.com>
2022-07-13 19:57:13 -0400

    added refresh button

--- public/index.js
+++ public/index.js
@@ -1,22 +1,36 @@
-function main(){
+async function main() {
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#reset-button');
 
-    let countValue = 0;
 
-    function increment(){
+    let data = await fetch("http://localhost:9001/counter")
+    let response = await data.json();
+    console.log(response)
+
+
+
+    let countValue = response.value;
+
+    function increment() {
         countValue++;
         countContainer.textContent = countValue;
     }
 
-    function decrement(){
+    function decrement() {
         countValue--;
         countContainer.textContent = countValue;
     }
 
+    function refresh() {
+        countContainer.textContent = response.value
+
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh);
     countContainer.textContent = countValue;
 }
 main()
